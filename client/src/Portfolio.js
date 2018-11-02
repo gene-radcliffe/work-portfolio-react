c7a97d6d739a05fdb5f5c64fda8f6ba7aa2d9939
gene-radcliffe <g_radcliffe@hotmail.com>
2018-11-02 15:51:44 -0400

    added S3, and environment settings

---
+++ client/src/Portfolio.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import 'spectre.css/dist/spectre.css';
+
+class Portfolio extends Component{
+
+    render(){
+
+        return(
+            <div className="hero container col-12">
+            
+            </div>
+        )
+    }
+
+}
+export default Portfolio;
