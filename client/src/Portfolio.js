ed7775053a11fb589020ee297f46474dcc91b980
gene-radcliffe <g_radcliffe@hotmail.com>
2018-11-10 13:39:53 -0500

    added React Router. Using HashHistory

--- client/src/Portfolio.js
+++ client/src/Portfolio.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import {HashRouter as Router, Route, Link} from "react-router-dom"
 import logo from './logo.svg';
+import resume from './Resume.pdf'
 import './App.css';
 import styled from 'styled-components';
 import 'spectre.css/dist/spectre.css';
@@ -10,6 +12,32 @@ import { faEnvelope }  from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 library.add(fab, faEnvelope)
 
+function Home(){
+    const size = "lg"
+    return(
+
+        <div className="hero">
+            
+        <Hero className="hero-box col-12">
+          <Span className="h1 hero-box_text text-center">gene radcliffe</Span>
+          <Span className="hero-box_text text-center" style={hero_sd_font}>software developer</Span> 
+          <div className="hero-social col-4"  >
+            <SocialLinks  size={size}/>
+          </div>
+          <Link to="/resume">Resume</Link>
+        </Hero>
+       </div> 
+    )
+}
+function Resume(){
+    let width = window.innerWidth
+    let height = window.innerHeight
+    
+    return(
+        <embed src={resume} width={width} height={height} />
+
+    )
+}
 const Span = styled.span`
     color:white;
     font-family: 'Rajdhani', sans-serif;
@@ -47,20 +75,16 @@ class Portfolio extends Component{
              size= "2x"
         }
         return(
-            <React.Fragment>
-            
-           <div className="hero">
-            
-            <Hero className="hero-box col-12">
-              <Span className="h1 hero-box_text text-center">gene radcliffe</Span>
-              <Span className="hero-box_text text-center" style={hero_sd_font}>software developer</Span> 
-              <div className="hero-social col-4"  >
-                <SocialLinks  size={size}/>
-              </div>
-            </Hero>
-           </div> 
+        
+            <Router>
+                <div id="index">
+                
+                <Route exact path="/" component={Home}></Route>
+                <Route path="/resume" component={Resume}></Route>
+                </div>
+            </Router>
 
-            </React.Fragment>
+        
 
         );
     }
